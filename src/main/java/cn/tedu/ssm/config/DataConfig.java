74eab03d75b7e81e3c9fb791ca60736871a2ac3d
chenjiabing666 <18796327106@163.com>
2019-08-05 10:18:04 +0800

    数据去除

--- src/main/java/cn/tedu/ssm/config/DataConfig.java
+++ src/main/java/cn/tedu/ssm/config/DataConfig.java
@@ -3,6 +3,6 @@ package cn.tedu.ssm.config;
 public interface DataConfig {
     String URL="jdbc:mysql://120.26.101.207:3306/gxrmyy?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull";
     String USER="root";
-    String PWD="Nov2014";
+    String PWD="****";
     String DRIVER_NAME="com.mysql.jdbc.Driver";
 }
